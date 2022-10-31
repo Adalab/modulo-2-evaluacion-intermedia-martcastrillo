fdf757b9495eb7b56fc0bb2b7cedf0c2d93b2a2e
Marta Castrillo <mart.castrillo@gmail.com>
2022-10-31 11:07:25 +0100

    add last ev commit

--- JS/main.js
+++ JS/main.js
@@ -17,34 +17,6 @@ const badRace5 = 5;
 function getRandomNumber(max) {
 	return Math.ceil(Math.random() * max);
 }
-
-function battle() {
-	const randomNumber = getRandomNumber(5);
-	const badRaceType = randomNumber;
-	const goodRaceTypeValue = parseInt(goodRaceType.value);
-    let winningGoods = 0;
-    let winningBads = 0;
-
-    ///luchando para que el contador de puntuación sume correctamente
-	if (badRaceType < goodRaceTypeValue) {
-		console.log(`LOS BUENOS ${goodRaceTypeValue} LOS MALOS ${badRaceType}`);
-		resultText.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";
-		badRacePoints.innerHTML = `La puntuación de la raza malvada es ${badRaceType}`;
-		playerPoints.innerHTML = winningGoods + 1;
-	} else if (badRaceType > goodRaceTypeValue) {
-		console.log(`LOS BUENOS ${goodRaceTypeValue} LOS MALOS ${badRaceType}`);
-		badRacePoints.innerHTML = `La puntuación de la raza malvada es ${badRaceType}`;
-		resultText.innerHTML =
-			"Ha ganado el Ejército del Mal! Vuelve a Intentarlo.";
-		computerPoints.innerHTML = winningBads + 1;
-	} else {
-		console.log(`LOS BUENOS ${goodRaceTypeValue} LOS MALOS ${badRaceType}`);
-		badRacePoints.innerHTML = `La puntuación de la raza malvada es ${badRaceType}`;
-		resultText.innerHTML = "EMPATE";
-	}
-	
-}
-
 ///contador de partidas
 let counter = 0;
 let numberOfTries = 0;
@@ -53,6 +25,35 @@ function clickCounter() {
 	numberOfTries = counter;
 	console.log(`Número de partidas jugadas ${numberOfTries}`);
 }
+let winningGoods = 0;
+let winningBads = 0;
+function battle() {
+	const randomNumber = getRandomNumber(5);
+	const badRaceType = randomNumber;
+	const goodRaceTypeValue = parseInt(goodRaceType.value);
+   
+
+    ///luchando para que el contador de puntuación sume correctamente
+	if (badRaceType < goodRaceTypeValue) {
+		console.log(`LOS BUENOS ${goodRaceTypeValue} LOS MALOS ${badRaceType}`);
+		resultText.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";
+		badRacePoints.innerHTML = `La puntuación de la raza malvada es ${badRaceType}`;
+		playerPoints.innerHTML = winningGoods +1;
+	} else if (badRaceType > goodRaceTypeValue) {
+		console.log(`LOS BUENOS ${goodRaceTypeValue} LOS MALOS ${badRaceType}`);
+		badRacePoints.innerHTML = `La puntuación de la raza malvada es ${badRaceType}`;
+		resultText.innerHTML =
+			"Ha ganado el Ejército del Mal! Vuelve a Intentarlo.";
+		computerPoints.innerHTML = winningBads +1;
+	} else {
+		console.log(`LOS BUENOS ${goodRaceTypeValue} LOS MALOS ${badRaceType}`);
+		badRacePoints.innerHTML = `La puntuación de la raza malvada es ${badRaceType}`;
+		resultText.innerHTML = "EMPATE";
+	}
+	
+}
+
+
 
 function handleClick(event) {
 	event.preventDefault();
